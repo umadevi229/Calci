d079737282542e420b8ea8856ddc09ef489838a2
umadevi229 <umadsastry05@gmail.com>
2019-09-23 11:53:51 +0530

    Calc Junit Test

---
+++ src/main/java/in/gov/isro/Calculator.java
@@ -0,0 +1,14 @@
+package in.gov.isro;
+
+public class Calculator {
+	public int add(int x ,int y)
+	{
+		return x+y;
+	}
+
+	public int multiply(int x ,int y)
+	{
+		return x*y;
+	}
+
+}
---
+++ src/test/java/in/gov/isro/TestCalc.java
@@ -0,0 +1,22 @@
+package in.gov.isro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalc {
+
+	@Test
+	public void testAdd() {
+		Calculator calc=new Calculator();
+		int result=calc.add(5,5);
+		assertEquals(10,result);
+	}
+
+	@Test
+	public void testmultiply() {
+		Calculator calc=new Calculator();
+		int result=calc.multiply(10,5);
+		assertEquals(50,result);
+	}
+}
