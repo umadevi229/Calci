1f8578fba3ce6500faf01baa984e34a3a905b8e3
umadevi229 <umadsastry05@gmail.com>
2019-09-23 12:05:49 +0530

    Substract feature added

--- src/main/java/in/gov/isro/Calculator.java
+++ src/main/java/in/gov/isro/Calculator.java
@@ -10,5 +10,8 @@ public class Calculator {
 	{
 		return x*y;
 	}
-
+	public int sub(int x ,int y)
+	{
+		return x-y;
+	}
 }
--- src/test/java/in/gov/isro/TestCalc.java
+++ src/test/java/in/gov/isro/TestCalc.java
@@ -19,4 +19,10 @@ public class TestCalc {
 		int result=calc.multiply(10,5);
 		assertEquals(50,result);
 	}
+	@Test
+	public void testsub() {
+		Calculator calc=new Calculator();
+		int result=calc.sub(10,5);
+		assertEquals(5,result);
+	}
 }
