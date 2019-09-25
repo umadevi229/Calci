c35ebb5cad51ff14c9c79111335cebc1bff80c19
umadevi229 <55570323+umadevi229@users.noreply.github.com>
2019-09-25 10:58:17 +0530

    Update Calculator.java

--- src/main/java/in/gov/isro/Calculator.java
+++ src/main/java/in/gov/isro/Calculator.java
@@ -1,18 +1,14 @@
 package in.gov.isro;
 
 public class Calculator {
-	public int add(int x ,int y)
-	{
+	public int add(int x ,int y)	{
 		return x+y;
 	}
 
-	public int multiply(int x ,int y)
-	{
-		int z=0;
-		return x*y;
+	public int multiply(int x ,int y)	{
+				return x*y;
 	}
-	public int sub(int x ,int y)
-	{
+	public int sub(int x ,int y)	{
 		return x-y;
 	}
 }
